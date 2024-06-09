ce3d96ff91a05b3ce93aab15cc346fdfa8a67c66
Jaanus Kääp <jaanus.kaap@gmail.com>
2024-06-09 21:49:33 +0300

    Shellcode example

---
+++ CommandEcho/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CommandEcho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating connection to the named pipe listener in agent (QQQWWWEEE is likely overwritten in reality)
+            NamedPipeClientStream client = new NamedPipeClientStream(".", "QQQWWWEEE", PipeDirection.InOut, PipeOptions.Asynchronous);
+            client.Connect();
+
+            //Supporting objects for reading and writing to the pipe
+            BinaryReader reader = new BinaryReader(client);
+            BinaryWriter writer = new BinaryWriter(client);
+
+            //Lets read the data from the pipe (4 byte size of the data, then the data)
+            int len = reader.ReadInt32();
+            byte[] data = reader.ReadBytes(len);
+
+            //Return same bytes back (0x30 is TLV type for "result data")
+            TLV tlv = new TLV(0x30, data);
+            data = tlv.getFullBuffer();
+            writer.Write(data.Length);
+            writer.Write(data);
+            writer.Flush();
+
+            //Return the "success" message
+            tlv = new TLV(0x33, new byte[0]);
+            data = tlv.getFullBuffer();
+            writer.Write(data.Length);
+            writer.Write(data);
+            writer.Flush();
+
+            //Close stuff up and wait 4sec just in case something is still being processed
+            client.WaitForPipeDrain();
+            Thread.Sleep(4 * 1000);
+            client.Close();
+        }
+    }
+}
---
+++ CommandEcho/TLV.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandEcho
+{
+    internal class TLV
+    {
+        public byte type { get; private set; } = 0;
+        public bool isParent { get; private set; } = false;
+        public Dictionary<byte, List<TLV>> children = null;
+        public byte[] data { get; private set; } = null;
+        public UInt32 fullSize { get; private set; } = 0;
+
+        public TLV()
+        {
+        }
+
+        public TLV(byte typeIn, byte[] dataIn)
+        {
+            type = typeIn;
+            data = dataIn;
+            isParent = false;
+            fullSize = (UInt32)data.Length + 5;
+        }
+
+        public TLV(byte typeIn)
+        {
+            type = typeIn;
+            isParent = true;
+            children = new Dictionary<byte, List<TLV>>();
+            fullSize = 5;
+        }
+
+        public bool load(byte[] buffer, int offset = 0)
+        {
+            if (buffer.Length - offset < 5)
+                return false;
+            type = (byte)(buffer[offset] & 0x7F);
+            isParent = (buffer[offset] & 0x80) > 0;
+            offset += 1;
+            UInt32 len;
+            bool iLittle = BitConverter.IsLittleEndian;
+            len = BitConverter.ToUInt32(buffer, offset);
+            offset += 4;
+            if (buffer.Length - offset < len)
+                return false;
+            fullSize = len + 5;
+
+            if (!isParent)
+            {
+                data = new byte[len];
+                Array.Copy(buffer, offset, data, 0, len);
+                return true;
+            }
+
+            children = new Dictionary<byte, List<TLV>>();
+            while (len != 0)
+            {
+                TLV child = new TLV();
+                if (!child.load(buffer, offset))
+                    return false;
+                addChild(child);
+                if (child.fullSize > len)
+                    return false;
+                offset += (int)child.fullSize;
+                len -= child.fullSize;
+            }
+            return true;
+        }
+
+        public bool addChild(TLV child)
+        {
+            if (!children.ContainsKey(child.type))
+                children.Add(child.type, new List<TLV>());
+            children[child.type].Add(child);
+            fullSize += child.fullSize;
+            return true;
+        }
+
+        public TLV getChild(byte childType, int idx)
+        {
+            if (!children.ContainsKey(childType))
+                return null;
+            return children[childType][idx];
+        }
+
+        public TLV getChild(byte childType)
+        {
+            return getChild(childType, 0);
+        }
+
+        public int getChildCount(byte childType)
+        {
+            if (!children.ContainsKey(childType))
+                return 0;
+            return children[childType].Count;
+        }
+
+        public byte[] getFullBuffer()
+        {
+            MemoryStream stream = new MemoryStream((int)fullSize);
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                getFullBuffer(writer);
+            }
+            return stream.ToArray();
+        }
+
+        private bool getFullBuffer(BinaryWriter writer)
+        {
+            writer.Write((byte)(type | (isParent ? 0x80 : 0)));
+            writer.Write((UInt32)(fullSize - 5));
+            if (!isParent)
+                writer.Write(data);
+            else
+                foreach (List<TLV> childArr in children.Values)
+                    foreach (TLV child in childArr)
+                        if (!child.getFullBuffer(writer))
+                            return false;
+            return true;
+        }
+
+        public String getDataAsString()
+        {
+            return System.Text.Encoding.UTF8.GetString(data);
+        }
+
+        public Int32 getDataAsInt32()
+        {
+            return BitConverter.ToInt32(data, 0);
+        }
+
+    }
+
+}
