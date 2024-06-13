8545f4daeb1e98aa60298c42f22044c5e68e97e0
Jaanus Kääp <jaanus.kaap@gmail.com>
2024-06-14 00:03:32 +0300

    Fixed a TLV input handling and added some error handling

--- shellcodes/CommandEcho/Program.cs
+++ shellcodes/CommandEcho/Program.cs
@@ -27,21 +27,52 @@ namespace CommandEcho
             int len = reader.ReadInt32();
             byte[] data = reader.ReadBytes(len);
 
-            //Return same bytes back (0x30 is TLV type for "result data")
-            TLV tlv = new TLV(0x30, data);
-            data = tlv.getFullBuffer();
-            writer.Write(data.Length);
-            writer.Write(data);
-            writer.Flush();
+            try
+            {
+                //Parse the input that should be TLV structure with type 0x5 (configuration data type)
+                TLV tlv = new TLV();
+                if (!tlv.load(data))
+                {
+                    throw new Exception("Failed to load TLV data");
+                }
+                if(tlv.type != 5)
+                {
+                    throw new Exception("TLV type is not 5 but " + (int)tlv.type);
+                }
+                byte[] messageBytes = tlv.data;
 
-            //Return the "success" message
-            tlv = new TLV(0x33, new byte[0]);
-            data = tlv.getFullBuffer();
-            writer.Write(data.Length);
-            writer.Write(data);
-            writer.Flush();
+                //Return same bytes back as data (0x30 is TLV type for result data)
+                tlv = new TLV(0x30, messageBytes);
+                data = tlv.getFullBuffer();
+                writer.Write(data.Length);
+                writer.Write(data);
+                writer.Flush();
+
+                //Return the "successful" status message (0x33 is TLV type for successful command status)
+                tlv = new TLV(0x33, new byte[0]);
+                data = tlv.getFullBuffer();
+                writer.Write(data.Length);
+                writer.Write(data);
+            }
+            catch (Exception e)
+            {
+                //Return exception error data (0x32 is TLV type for error data)
+                byte[] exceptionBytes = Encoding.UTF8.GetBytes(e.Message);
+                TLV tlv = new TLV(0x32, exceptionBytes);
+                data = tlv.getFullBuffer();
+                writer.Write(data.Length);
+                writer.Write(data);
+                writer.Flush();
+
+                //Return the "error" status message(0x34 is TLV type for failed command status)
+                tlv = new TLV(0x34, new byte[0]);
+                data = tlv.getFullBuffer();
+                writer.Write(data.Length);
+                writer.Write(data);
+            }
 
             //Close stuff up and wait 4sec just in case something is still being processed
+            writer.Flush();
             client.WaitForPipeDrain();
             Thread.Sleep(4 * 1000);
             client.Close();
